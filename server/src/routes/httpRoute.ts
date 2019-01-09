f961c96dee278bbb2f4e3fff32c92ce771c8f1db
九条研介 <dinopresent@qq.com>
2019-01-09 16:45:39 +0800

    处理了token

--- server/src/routes/httpRoute.ts
+++ server/src/routes/httpRoute.ts
@@ -1,47 +1,24 @@
 import * as express from "express";
-
-import config from "../config";
-import loger from "../logIns";
-import Database from "../mongo";
-
 // 路由
 import testHandle from "./testHandle";
-
 // 错误
 import { ErrCode } from "../errCode";
-import { json } from "body-parser";
+// jwt
 import JwtService from "../service/jwtService";
 
 export default function handler(app: express.Express) {
   // token
-  // app.use(
-  //   "/user/",
-  //   jwt({ secret: config.jwt.secret }),
-  //   (err, req: express.Request, res: express.Response, next) => {
-  //     if (err) {
-  //       res.status(500).json(ErrCode.invalidToken);
-  //       return;
-  //     }
-  //     next();
-  //   }
-  // );
-  // app.use(async (req, res, next) => {
-  //   if (/^\/user\//.test(req.path)) {
-  //     let token: string = req.headers["token"] as string;
-  //     JwtService.verify(token)
-  //     let service = await TokenService.getIns();
-  //     let checkRst = await service.check(token);
-  //     console.log({ token, checkRst });
-  //     console.log(JSON.stringify(token));
-  //     if (!checkRst) {
-  //       res.json(ErrCode.invalidToken);
-  //       return;
-  //     }
-  //     let info = await service.getInfo(token);
-  //     req.headers["openId"] = info.openId;
-  //   }
-  //   next();
-  // });
+  app.use("/user/", (req: express.Request, res: express.Response, next) => {
+    if (/^\/user\//.test(req.path)) {
+      let token: string = req.headers["token"] as string;
+      let decode = JwtService.verify(token);
+      if (!decode || decode.expires < Date.now()) {
+        res.json(ErrCode.invalidToken);
+        return;
+      }
+    }
+    next();
+  });
 
   // 测试
   testHandle(app);
