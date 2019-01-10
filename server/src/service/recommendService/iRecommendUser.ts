ffaebf4d6eb02e6aa828a441e8b31ab2f501a171
九条研介 <dinopresent@qq.com>
2019-01-10 17:36:57 +0800

    ~

--- server/src/service/recommendService/iRecommendUser.ts
+++ server/src/service/recommendService/iRecommendUser.ts
@@ -1,1 +1,12 @@
-export default interface IRecommendUser {}
+export default interface IRecommendUser {
+  // 图片url
+  photoUrl: string;
+  // 昵称
+  nickname: string;
+  // 生年
+  birth: number;
+  // 城市
+  city: string;
+  // 日记数量
+  dailyCount: number;
+}
