c630d13495f9fc0977e162e247e99c528e779c23
tongjinle <1328192478@qq.com>
2019-01-13 19:58:01 +0800

    完成tokenService的测试

--- server/src/service/jwtService/index.ts
+++ server/src/service/jwtService/index.ts
@@ -5,11 +5,24 @@ import IToken from "./iToken";
 const JWT_SECRET: string = config.jwt.secret;
 
 export default class JwtService {
+  /**
+   * 给明文生成一段token
+   * @param  {IToken} info 明文
+   * @returns string token密文
+   */
   static sign(info: IToken): string {
     return jwt.sign(info, JWT_SECRET);
   }
-
+  /**
+   * 解密token
+   * @param  {string} token
+   * @returns IToken 解密的token信息,如果解密失败,则返回undefined
+   */
   static verify(token: string): IToken {
-    return jwt.verify(token, JWT_SECRET) as IToken;
+    try {
+      return jwt.verify(token, JWT_SECRET) as IToken;
+    } catch (e) {
+      return undefined;
+    }
   }
 }
--- server/src/test/simple/token.test.ts
+++ server/src/test/simple/token.test.ts
@@ -1,10 +1,14 @@
 import assert = require("assert");
 
-// var assert = require("power-assert");
 import jwtService from "../../service/jwtService";
 import iToken from "../../service/jwtService/iToken";
 describe("token", () => {
-  it("jwt-token", () => {
+  // token的创建和解密
+  // process:
+  // 创建一个'tongjinle',expries为2000-1-1的token
+  // check:
+  // 查看解密的信息,userId是否是'tongjinle',expires是否是2000-1-1的时间戳
+  it("jwt-token", function() {
     let info: iToken = {
       userId: "tongjinle",
       expires: new Date(2000, 0, 1).getTime()
@@ -12,6 +16,20 @@ describe("token", () => {
     let token: string = jwtService.sign(info);
 
     let decodeInfo = jwtService.verify(token);
-    assert(decodeInfo.userId === "tongjinle");
+    assert(
+      decodeInfo.userId === "tongjinle" &&
+        decodeInfo.expires === new Date(2000, 0, 1).getTime()
+    );
+  });
+
+  // 不合法的token的解密
+  // process:
+  // 解密一个不合法的token
+  // check:
+  // 解密出来的信息是undefined(即解密有错)
+  it("invaild-token", function() {
+    let token: string = "abc";
+    let decodeInfo = jwtService.verify(token);
+    assert(!decodeInfo);
   });
 });
